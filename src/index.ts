f55be551ee79a4597258c4b831adb72d812c0c78
Polyfox <tr808axm@gmail.com>
2019-12-30 12:37:38 +0100

    feat(endpoints): implemented /current_user (#2)
    This implements the current_user route and User data parsing.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Campaign } from './models/campaign';
+export { Pledge } from './models/pledge';
+export { User, SocialConnections } from './models/user';
+
+export { PatreonAPI } from './patreonApi';
---
+++ src/patreonApi.ts
@@ -0,0 +1,51 @@
+import * as request from 'request';
+import { User } from '.';
+
+/**
+ * Wrapper for the Patreon API.
+ * @see https://docs.patreon.com/ API documentation
+ */
+export class PatreonAPI {
+  readonly BASE_URL: string = 'https://www.patreon.com/api/oauth2/api';
+  /**
+   * The access token used to authorize API requests.
+   */
+  accessToken: string;
+
+  /**
+   * Creates a new Patreon API wrapper instance.
+   * @param accessToken the token to use for API request authorization.
+   */
+  constructor(accessToken: string) {
+    this.accessToken = accessToken;
+  }
+
+  /**
+   * 
+   */
+  async getCurrentUser(): Promise<User> {
+    const body = await this.buildRequest('/current_user');
+    return User.parse(body);
+  }
+
+  /**
+   * Executes a request.
+   * @param route API endpoint, must start with a `/`
+   */
+  private buildRequest(route: string): Promise<object> {
+    return new Promise<object>((resolve, reject) => {
+      request({
+        url: this.BASE_URL + route,
+        headers: {
+          'Authorization': `Bearer ${this.accessToken}`,
+        }
+      }, (err, res, body) => {
+        if (!err && res.statusCode == 200) {
+          resolve(JSON.parse(body));
+        } else {
+          reject(err);
+        }
+      });
+    });
+  }
+}
