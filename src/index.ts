f71c176b7d4659eb559ee57501fd2bc3a0465d6e
traxam <tr808axm@gmail.com>
2020-01-02 19:11:21 +0100

    feat(models): export Reward and Goal in index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export { Campaign } from './models/campaign';
 export { Pledge } from './models/pledge';
 export { User, SocialConnections } from './models/user';
+export { Reward } from './models/reward';
+export { Goal } from './models/goal';
 
 export { PatreonAPI } from './patreonApi';
 export { Page } from './page';
