265ad4a9470dc48511267de08e6d94eed724e325
traxam <tr808axm@gmail.com>
2020-01-03 19:04:06 +0100

    feat(models): added Address attributes (#9)

--- src/models/address.ts
+++ src/models/address.ts
@@ -8,7 +8,44 @@ import { DataStore } from "../dataStore";
  * @see https://docs.patreon.com/#address APIv2 documentation
  */
 export class Address extends PatreonObject {
-  //todo attributes
+  /**
+   * Full recipient name, can be null.
+   */
+  addressee: string;
+  /**
+   * First line of the street address, can be null.
+   * @see line2
+   */
+  line1: string;
+  /**
+   * Second line of the street address, can be null.
+   * @see line1
+   */
+  line2: string;
+  /**
+   * Postal or zip code, can be null.
+   */
+  postalCode: string;
+  /**
+   * Name of the city.
+   */
+  city: string;
+  /**
+   * State or province name, can be null.
+   */
+  state: string;
+  /**
+   * Country name.
+   */
+  country: string;
+  /**
+   * Telephone number, specified for non-US addresses. Can be null.
+   */
+  phoneNumber: string;
+  /**
+   * Datetime address was first created.
+   */
+  createdAt: Date;
 
   /**
    * Constructs a new pledge object.
